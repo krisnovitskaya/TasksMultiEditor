544738833c024335c0c8c3710c98921a5d925aa7
krisnovitskaya <voitage@yandex.ru>
2023-04-23 22:46:40 +0300

    Initial commit

---
+++ src/test/java/ru/krisnovitskaya/TasksMultiEditor/TasksMultiEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krisnovitskaya.TasksMultiEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksMultiEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
