e0d47cf397a7b1a7f67284d484de55e10c35c7fb
krisnovitskaya <voitage@yandex.ru>
2023-05-01 12:07:58 +0300

    add history table and trigger

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -35,3 +35,39 @@ create table if not exists tasks(
     foreign key (executor_id) references users (id),
     foreign key (controller_id) references users (id)
     );
+
+create table if not exists tasks_history(
+    id              	bigserial primary key,
+    operation           varchar(10),
+    task_id             bigint not null,
+    executor_id_new     bigint,
+    controller_id_new   bigint,
+    title_new           varchar(255),
+    description_new     text not null,
+    deadline_new        date not null,
+    version            	int not null,
+    modified_by   	    varchar(100),
+    modified_date       timestamp
+    );
+
+CREATE OR REPLACE FUNCTION tasks_history() RETURNS trigger AS
+$BODY$
+BEGIN
+    IF TG_OP = 'UPDATE' THEN
+        INSERT INTO tasks_history(operation, task_id, executor_id_new, controller_id_new, title_new, description_new, deadline_new, version, modified_by, modified_date)
+        VALUES ('UPDATE', old.id, new.executor_id, new.controller_id, new.title, new.description, new.deadline, new.version, new.last_modified_by, current_timestamp);
+        RETURN NEW;
+
+    ELSIF TG_OP = 'INSERT' THEN
+        INSERT INTO tasks_history(operation, task_id, executor_id_new, controller_id_new, title_new, description_new, deadline_new, version, modified_by, modified_date)
+        VALUES ('INSERT', new.id, new.executor_id, new.controller_id, new.title, new.description, new.deadline, new.version, new.last_modified_by, current_timestamp);
+        RETURN NEW;
+    END IF;
+    RETURN NULL;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER history_tasks
+    AFTER INSERT OR UPDATE ON tasks
+    FOR EACH ROW EXECUTE PROCEDURE tasks_history();
